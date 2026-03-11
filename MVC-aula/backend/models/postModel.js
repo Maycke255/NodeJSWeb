dca5729141331c53a944241b359cddeb15009167
Maycke <reydsonk@gmail.com>
2026-03-10 22:10:52 -0300

    ....

---
+++ MVC-aula/backend/models/postModel.js
@@ -0,0 +1,7 @@
+/* Vamos começar com o model, o model nada mais e que o mediador entre o banco de dados e o config, e toda a configuração de méotodos e dados */
+
+class PostModel {
+    constructor() {
+        this.posts = [];
+    }
+}
