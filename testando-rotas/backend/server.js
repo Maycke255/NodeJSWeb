efd8051c7304cf4aff6748a4e67ec44afcfebb47
Maycke <reydsonk@gmail.com>
2026-03-26 23:09:04 -0300

    ...

--- testando-rotas/backend/server.js
+++ testando-rotas/backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const router = require('./routes/routes.js')
+const router = require('./routes/routes.js');
+const path = require('path');
+
 app.use(express.json());
+
+app.use(express.static([path.join(__dirname, '..', 'frontend')]));
 
 app.use(router);
 
