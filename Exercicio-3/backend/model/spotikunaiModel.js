13c9441f4b5b9c4ea39de7d67e62d89b4805a531
Maycke <reydsonk@gmail.com>
2026-03-31 18:35:05 -0300

    Parei no metodo PUT

--- Exercicio-3/backend/model/spotikunaiModel.js
+++ Exercicio-3/backend/model/spotikunaiModel.js
@@ -1,5 +1,87 @@
+const crypto = require('crypto');
+
 class SpotikunaiModel {
     constructor () {
-        this.playlists = [];
+        this.playlists = [
+            //Exemplo de como poderia ser uma playlist:
+            {
+                playlistID: '1',
+                name: 'Sertanejo',
+                tags: ['Sertanejo romantico', 'Clássico', 'Raiz', 'Modão', 'Sofrencia', 'Dançar Coladinho', 'Apaixonante'],
+                //Lista onde ira conter todas as musicas
+                playlist: [
+                    {
+                        songID: '1',
+                        title: 'Dou a vida por um beijo',
+                        year: 2000,
+                        artist: 'Zezé Di Camargo & Luciano',
+                        album: 'Flores em vida'
+                    },
+                    {
+                        songID: '2',
+                        title: 'Evidências',
+                        year: 1990,
+                        artist: 'Chitãozinho e Xororó',
+                        album: 'Cowboy do asfalto' 
+                    }
+                ]
+            },
+            {
+                playlistID: '2',
+                name: 'Anime RAP',
+                tags: ['Rap de anime', 'Geek', 'Trap de anime', 'Rap Nerd', 'Batalha', 'Protagonistas', 'Vilões', 'Motivacional', 'Frestayle Anime'],
+                //Lista onde ira conter todas as musicas
+                playlist: [
+                    {
+                        songID: '1',
+                        title: 'Rap do Naruto',
+                        year: 2020,
+                        artist: '7 Minutoz',
+                        album: 'Rap de Anime'
+                    },
+                    {
+                        songID: '2',
+                        title: 'Rap do Levi',
+                        year: 2021,
+                        artist: 'Player Tauz',
+                        album: 'Attack on Titan Rap'
+                    }
+                ]
+            }
+        ];
+    }
+
+    //-- GET -- OBTER TODA A LISTA DE PLAYLIST
+    allPlaylists () {
+        if (this.playlists.length === 0) {
+            return { success: true, message: 'Você não tem nenhuma playlist criada no momento.' };
+        }
+
+        return { success: true, data: this.playlists };
+    }
+
+    // -- POST -- CADASTRAR NOVA PLASYLIST
+    createNewPlaylist (name) {
+        const newPlaylist = {
+            playlistID: crypto.randomUUID(),
+            name: name,
+            tags: [],
+            playlist: []
+        }
+
+        this.playlists.push(newPlaylist);
+        return { success: true, data: newPlaylist, message: 'Nova Playlist criada com sucesso!' }
+    }
+
+    // -- PUT -- ATUALIZAR NOME E TAGS DA LISTA
+    updatePost (id, updates) {
+        const index = this.playlists.findIndex(playlist => playlist.playlistID === id);
+
+        if (index === -1) {
+            return { success: false, data: [], message: 'Playlist informada indexistente.' };
+        }
+
+        this.playlists[index] = { ...this.playlists[index], ...updates };
+        return { success: true, data: this.playlists[index], message: `Dados da playlist ${this.playlists[index].name} atualizados com sucesso!` };
     }
 }
--- Exercicio-3/backend/server.js
+++ Exercicio-3/backend/server.js
@@ -2,6 +2,7 @@
 
 Uma playlist deve ter, pelo menos, nome e uma lista de tags para classificá-la.
 Uma playlist deve ter uma lista de músicas, onde cada música deve ter, no mínimo, título, ano, artista e álbum.
+
 Deve ser possível obter uma lista de todas as playlists existentes, bem como uma playlist individualmente.
 Deve ser possível cadastrar novas playlists, com ou sem músicas nela.
 Deve ser possível atualizar o nome e a lista de tags de uma playlist.
