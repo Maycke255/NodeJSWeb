206b32b61cb802f8c8ba1228c779235aa1d3e7fc
Maycke <reydsonk@gmail.com>
2026-04-08 22:55:58 -0300

    ...

--- middlewares-aula/model/model.js
+++ middlewares-aula/model/model.js
@@ -16,13 +16,37 @@ class ModelMuter {
         ]
     }
 
-    register (username, password) {
-        const userAlreadyExists = this.users.findIndex(user => user.username === username);
+    //POST register
+    register (id, username, password) {
+        const userAlreadyExists = this.users.findIndex(userId => userId.id === id);
 
         if (userAlreadyExists) {
             return { success: false, message: 'Usuario já cadastrado', data: [] }
         }
 
-        
+        const newUser = { username, password }
+        this.users.push(newUser);
+
+        return { success: true, message: 'Usuario cadastrado com sucesso', data: this.users }
     }
+
+    //POST login
+    login (id, username, password) {
+        const userAlreadyExists = this.users.findIndex(userId => userId.id === id);
+
+        if (userAlreadyExists === -1) {
+            return { success: false, message: 'Usuario não cadastrado!', data: [] }
+        }
+
+        const userPassword = this.users[userAlreadyExists].password;
+        if (password !== userPassword) {
+            return { success: false, message: 'Senha incorreta', data: [] }
+        }
+
+        return { success: true, message: 'Login efetuado com sucesso', data: { user: username, password: password } }
+    }
+
+    //GET logout
 }
+
+module.exports = new ModelMuter();
